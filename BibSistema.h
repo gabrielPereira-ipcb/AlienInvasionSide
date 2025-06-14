edbfab58a19ff120a5a5459e9e814a88215dc849
Gabriel De Santana Pereira <141341655+gabrielPereira-ipcb@users.noreply.github.com>
2025-06-14 14:10:10 +0100

    Update BibSistema.h

--- BibSistema.h
+++ BibSistema.h
@@ -1,29 +1,31 @@
 // BibSistema.h
 #pragma once
 
-#include <iostream>   // Para entrada e saída padrão (como console)
-#include <vector>     // Para usar vetores dinâmicos, por exemplo, para listas de inimigos ou balas 
-#include <stdlib.h>   // Necessário para funções como exit()
-#include <cmath>      // Para funções matemáticas comuns em computação gráfica (e.g., cálculos de distância, ângulos)
-#include <sstream>    // Pode ser útil para formatação de texto, como exibir a pontuação ou vidas (embora não explicitamente mencionado para este arquivo, é uma biblioteca padrão útil)
+#include <iostream>   
+#include <vector>     
+#include <stdlib.h>   
+#include <cmath>      
+#include <sstream>    
 
 // Detecção de sistema operacional para incluir a biblioteca gráfica correta
-
-
-#ifdef __APPLE__
-    #include <GLUT/glut.h>
-#elif defined(_WIN32) || defined(_WIN64)
-    #include <GL/gl.h>
-    #include <GL/glu.h>
+#if defined(_WIN32) || defined(_WIN64)
+    #include <GL\freeglut.h>
+#elif defined(__APPLE_) || defined(__MACH__) || defined (TAREGT_OS_MAC)
+    #include <OpenGL/OpenGL.h>
+    #include <GLUT/GLUT.h>
 #elif defined(__linux__)
     #include <GL/glut.h>
 #endif
 
-// cabeçalhos
-// Declarar variáveis globais ou outras definições partilhadas aqui, se necessário.
-extern GLfloat tamanhoDoMundo;
-extern GLfloat coordenadasMundo[4];
-extern GLfloat naveJogadorTamanho;
 
+
+//variáveis globais ou outras definições partilhadas 
+
+extern GLfloat tamanhoMundo;
+extern GLfloat coordenadasMundo[4];
+extern GLfloat naveJogadorTamanho[2];
+extern GLfloat naveInimigaTamanho[2];
+extern GLfloat tamanhoProjetil;
+extern GLfloat escala;
 
 
